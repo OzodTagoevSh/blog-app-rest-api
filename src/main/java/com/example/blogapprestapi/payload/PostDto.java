9756b11287721d1394f183c65d8a24ebb545bd09
Ozod Tagoev <33785422+OzodTagoevSh@users.noreply.github.com>
2023-04-16 16:11:46 -0500

    dto for post entity to create a connection between client and rest apis

---
+++ src/main/java/com/example/blogapprestapi/payload/PostDto.java
@@ -0,0 +1,17 @@
+package com.example.blogapprestapi.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class PostDto {
+    private Long id;
+    private String title;
+    private String description;
+    private String content;
+}
